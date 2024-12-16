d9996b5a8e8fd701fb6bbed2b36e6142d78d8f91
Thomas A Caswell <tcaswell@bnl.gov>
2024-12-16 15:54:48 -0500

    MNT: version bump + add Python classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(name='historydict',
-      version='1.2.6',
+      version='1.2.7',
       author='Brookhaven National Laboratory',
       py_modules=['historydict'],
       description='A persistent dictionary with history backed by sqlite',
       url='http://github.com/Nikea/historydict',
       platforms='Cross platform (Linux, Mac OSX, Windows)',
       license_files=('LICENSE.',),
+      classifiers = [
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
+          "Programming Language :: Python :: 3.13",
+      ]
       )
